805f0188e978c31598245aebc94203787d4c40eb
sean chan <sean@seans-MacBook-Air.local>
2018-10-23 23:17:40 +0800

    Added functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,6 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped queen");
-console.log("User flipped king");
-
-if(cardsInPlay.length === 2){
+var checkForMatch = function (){
     if(cardsInPlay[0] === cardsInPlay[1]){
         alert("You found a match!");
     }
@@ -16,3 +8,13 @@ if(cardsInPlay.length === 2){
         alert("Sorry, try again.");
     }
 }
+var flipCard = function (cardId){
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    
+    if(cardsInPlay.length === 2){
+        checkForMatch();
+    }
+}
+flipCard(0);
+flipCard(2);
