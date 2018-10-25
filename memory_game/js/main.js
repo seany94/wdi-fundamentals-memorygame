6648efcb8d75684f5897150af82bd875588dd46c
sean chan <sean@seans-MacBook-Air.local>
2018-10-25 23:15:41 +0800

    added Objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "../images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamond",
+        cardImage: "../images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "../images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "../images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 var checkForMatch = function (){
     if(cardsInPlay[0] === cardsInPlay[1]){
@@ -9,9 +30,10 @@ var checkForMatch = function (){
     }
 }
 var flipCard = function (cardId){
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
-    
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
     if(cardsInPlay.length === 2){
         checkForMatch();
     }
