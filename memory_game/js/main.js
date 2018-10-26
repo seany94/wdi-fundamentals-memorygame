11aa5814db7e105cac797ccad06119fb12e8f207
sean chan <sean@seans-MacBook-Air.local>
2018-10-27 01:26:05 +0800

    final with reset btn

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -29,14 +29,30 @@ var checkForMatch = function (){
         alert("Sorry, try again.");
     }
 }
-var flipCard = function (cardId){
+var flipCard = function (){
+    var cardId = this.getAttribute('data-id');
     console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
     console.log(cards[cardId].suit);
     console.log(cards[cardId].cardImage);
+    this.setAttribute('src', cards[cardId].cardImage);
     if(cardsInPlay.length === 2){
         checkForMatch();
     }
 }
-flipCard(0);
-flipCard(2);
+
+var createBoard = function(){
+    for(i = 0; i < cards.length; i++){
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', "../images/back.png");
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+createBoard();
+
+var resetGame = function(){
+    window.location.reload(); //this was the best i can do by just refreshing page
+}
