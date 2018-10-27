f2c97f1f5237fed30c9ccb458c2c27788f98e371
sean chan <sean@seans-MacBook-Air.local>
2018-10-28 00:06:05 +0800

    final with update resetBtn n score

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,9 +21,12 @@ var cards = [
     }
 ];
 var cardsInPlay = [];
+var score = document.getElementById("score").innerHTML = 0; //variable to store score
 var checkForMatch = function (){
     if(cardsInPlay[0] === cardsInPlay[1]){
         alert("You found a match!");
+        score +=1; //score increment for every correct pair
+        document.getElementById("score").innerHTML = score; //added score onto html
     }
     else{
         alert("Sorry, try again.");
@@ -49,10 +52,24 @@ var createBoard = function(){
         cardElement.addEventListener('click', flipCard);
         document.getElementById('game-board').appendChild(cardElement);
     }
+    /*document.getElementsByTagName('div').innerHTML = null; //not working as intended cant clear cards*/
 }
 
 createBoard();
 
 var resetGame = function(){
-    window.location.reload(); //this was the best i can do by just refreshing page
+    cardsInPlay = [];
+    createBoard();
 }
+
+document.getElementById('reset').addEventListener('click', resetGame);
+
+
+//var score = document.getElementById('score').innerHTML = "Score: <score>";
+
+/*var resetGame = function(){
+        for (i = 0; i < cards.length; i++) {
+        document.getElementById('game-board')[0].remove();
+      }
+    window.location.reload(); //this was the best i can do by just refreshing page
+}*/
